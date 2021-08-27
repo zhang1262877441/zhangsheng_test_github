364fb1d291044164c56603dc57f9935b96966949
zhang1262877441 <61043569+zhang1262877441@users.noreply.github.com>
2021-08-27 14:54:33 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+{"model_path": "/data/zs/data_handtrack/model/SiamRPNPPRes50_130.pth",
+    "model_name": "siam_onnx",
+    "input_size":"(1920,1080)",
+    "Templepp_onnx":"/data/zs/data_handtrack/onnx_model/temple_onnx.onnx",
+    "SearchPre_onnx":"/data/zs/data_handtrack/onnx_model/searchPre_onnx.onnx",
+    "SearchPost_onnx":"/data/zs/data_handtrack/onnx_model/searchPost_onnx.onnx"
+   }
